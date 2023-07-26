9836bfa91f2c650cde2e8c73f68f594d066cbc5e
Manikanta12344 <139945777+Manikanta12344@users.noreply.github.com>
2023-07-26 16:07:19 +0530

    Update swap.java

--- src/main/java/com/dailycode/swap.java
+++ src/main/java/com/dailycode/swap.java
@@ -2,8 +2,8 @@ package com.dailycode;
 
 public class swap {
     public static void main(String[] args) {
-        String s1 = "mani";
-        String s2 = "kanta";
+        String s1 = "manikanta";
+        String s2 = "chinimilli";
 
         String s3 = s1.concat(s2);
 

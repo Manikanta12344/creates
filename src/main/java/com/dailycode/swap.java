0f22bd3ac81ad24250855aed9a607ce92ab53079
Manikanta12344 <chinimillimanikanta1234@gmail.com>
2023-07-26 16:01:07 +0530

    Initial commit

---
+++ src/main/java/com/dailycode/swap.java
@@ -0,0 +1,12 @@
+package com.dailycode;
+
+public class swap {
+    public static void main(String[] args) {
+        String s1 = "mani";
+        String s2 = "kanta";
+
+        String s3 = s1.concat(s2);
+
+        System.out.println(s3);
+    }
+}

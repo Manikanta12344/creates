a3b742455ddde2b99ec3f2001c8a0d49cb7e9fbf
Manikanta12344 <139945777+Manikanta12344@users.noreply.github.com>
2023-07-26 16:13:21 +0530

    Update swap.java
    adding another names

--- src/main/java/com/dailycode/swap.java
+++ src/main/java/com/dailycode/swap.java
@@ -4,6 +4,12 @@ public class swap {
     public static void main(String[] args) {
         String s1 = "manikanta";
         String s2 = "chinimilli";
+        String s3="sri";
+        String s4="sai";
+
+        String s5=s3.concat(s4);
+
+        System.out.println(s5);
 
         String s3 = s1.concat(s2);
 
